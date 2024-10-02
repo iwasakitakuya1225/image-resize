1d1c75692d54b2cfc9c84fc60ec8171aabff974d
iwasakitakuya1225 <46528567+iwasakitakuya1225@users.noreply.github.com>
2024-10-02 16:00:29 +0900

    losslessに対応

--- origin-request/index.js
+++ origin-request/index.js
@@ -122,11 +122,11 @@ exports.handler = async (event, context, callback) => {
     sharpBody.resize(resizeWidth, resizeHeight).rotate()
     if (isAvif) {
       format = 'avif'
-      sharpBody.avif({ quality })
+      sharpBody.avif({ quality, lossless: true })
     } else if (isWebp) {
       // webp対応しているブラウザの場合はwebpにする
       format = 'webp'
-      sharpBody.webp({ quality })
+      sharpBody.webp({ quality, lossless: true })
     } else if (format === 'png') {
       quality = 10 - parseInt(quality / 10)
       sharpBody.png({ compressionLevel: quality })
